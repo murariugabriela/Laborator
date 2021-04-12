19c81b31c9ef60c8fd6c38a9af90d4a590912c52
murariugabriela <79075813+murariugabriela@users.noreply.github.com>
2021-04-12 07:52:27 +0300

    Update Player.java

--- Laborator7/Optional/Player.java
+++ Laborator7/Optional/Player.java
@@ -4,11 +4,19 @@ import javafx.util.Pair;
 
 import java.util.Vector;
 
-public class Player {
+public class Player{
     private String name;
     private Vector<Pair<Integer, Integer>> tokenSequence = new Vector<>();
     private int playerNumber;
     private int playerScore;
+
+    public void setPlayerNumber(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -46,4 +54,8 @@ public class Player {
     public Vector<Pair<Integer, Integer>> getTokenSequence() {
         return tokenSequence;
     }
+
+//    @Override
+//    public void run() {
+//    }
 }
